7cc571d80d59e277d088b32188f96b5c4cf2e3dc
Sohit <sohit@digitalgreen.org>
2023-08-25 14:34:23 +0530

    modified file name

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TodoItemViewSet
-from todo_project import settings
+from core import settings
 
 router = DefaultRouter()
 router.register(r'todos', TodoItemViewSet, basename='todos')
