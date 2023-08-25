e1e96bc1541d6820794bc39e5bbca00f3b8226e4
Sohit <sohit@digitalgreen.org>
2023-08-25 15:46:33 +0530

    id to uuid

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class TodoItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
 
