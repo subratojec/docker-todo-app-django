34d98ed19c35987195d5dc0dc4684e17cf3eda07
Sohit <sohit@digitalgreen.org>
2023-09-14 16:16:28 +0530

    (modify) : port correction in settings.py file

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5431"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         "OPTIONS": {
             "client_encoding": "UTF8",
         },
