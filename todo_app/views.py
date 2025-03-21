5d3902a07f6062610f1c9af0abe8a3b692b57588
Subrato Singh <120516562+subratojec@users.noreply.github.com>
2025-03-22 01:35:00 +0530

    Update views.py

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,19 +1,41 @@
-# todo_app/views.py
 import logging
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.mixins import DestroyModelMixin
 from .models import TodoItem
 from .serializers import TodoItemSerializer
-from rest_framework.viewsets import GenericViewSet, ViewSet
-
+from rest_framework.viewsets import GenericViewSet
 
 LOGGER = logging.getLogger(__name__)
+
+# View to render the index.html template with the list of todos
+def index_view(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        if title:
+            TodoItem.objects.create(title=title)
+        return redirect('index')  # Redirect to the same page after adding
+    todos = TodoItem.objects.all()
+    return render(request, 'index.html', {'todos': todos})
+
+# View to delete a todo item
+def delete_todo(request, pk):
+    todo = get_object_or_404(TodoItem, pk=pk)
+    if request.method == "POST":
+        todo.delete()
+        return redirect('index')
+    return redirect('index')
+
+# View to view a single todo item (optional, for the "View" link)
+def todo_detail(request, pk):
+    todo = get_object_or_404(TodoItem, pk=pk)
+    return render(request, 'todo_detail.html', {'todo': todo})
+
+# Existing API ViewSet
 class TodoItemViewSet(GenericViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
     
-    # Custom create method
     def create(self, request, *args, **kwargs):
         try:
             serializer = self.get_serializer(data=request.data)
@@ -21,24 +43,22 @@ class TodoItemViewSet(GenericViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
-            LOGGER.error(e,exc_info=True)
+            LOGGER.error(e, exc_info=True)
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    # Custom destroy method
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.delete()
         LOGGER.info(f"Deleted task {instance.title}")
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    # Custom update method
     def update(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             serializer = self.get_serializer(instance, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             LOGGER.error(e)
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -50,10 +70,9 @@ class TodoItemViewSet(GenericViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             LOGGER.error(e)
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    # Custom get method
-    def get(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs):  # Renamed 'get' to 'retrieve' to match DRF convention
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
